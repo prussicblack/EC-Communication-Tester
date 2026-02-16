c5c660707b4667910d5f3f5ad7fdaaf2c53d8628
ursamizar <prussicblack@gmail.com>
2026-02-16 18:11:38 +0900

    PDO작업중 저장.

--- SOEM_FrontEnd/Ethercat/EthercatProfile/Interfaces/IPDOView.cs
+++ SOEM_FrontEnd/Ethercat/EthercatProfile/Interfaces/IPDOView.cs
@@ -8,6 +8,10 @@ using Avalonia.Platform;
 
 namespace SOEM_FrontEnd.Ethercat.EthercatProfile.Interfaces
 {
+    //외부에서 PDO상태를 보기 위한 인터페이스.
+
+    //스냅샷 프로퍼티로 노출하고, PDO루프에서 Publish해줘야 됨.
+
     public interface IPDOView
     {
         ReadOnlyMemory<byte> RxSnapshot { get; }
