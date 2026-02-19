d91c1699669f9e52d8d7e82cfb58af3543bb1280
prussicblack <prussicblack@gmail.com>
2026-02-19 18:07:28 +0900

    커스텀 타이틀바 적용

--- SOEM_FrontEnd/Views/MainWindow.axaml.cs
+++ SOEM_FrontEnd/Views/MainWindow.axaml.cs
@@ -1,6 +1,8 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
+using Avalonia.Layout;
 
 namespace SOEM_FrontEnd.Views;
 
@@ -8,10 +10,27 @@ public partial class MainWindow : Window
 {
     private AboutWindow _about;
 
+    public static readonly StyledProperty<Thickness> WindowPaddingProperty =
+       AvaloniaProperty.Register<MainWindow, Thickness>(nameof(WindowPadding));
+
+
+    public Thickness WindowPadding
+    {
+        get { return GetValue(WindowPaddingProperty); }
+        set { SetValue(WindowPaddingProperty, value); }
+    }
+
     public MainWindow()
     {
         InitializeComponent();
+        UpdatePadding();
+        this.PropertyChanged += (_, e) =>
+        {
+            if (e.Property == WindowStateProperty)
+                UpdatePadding();
+        };
     }
+
     private void About_Click(object? sender, RoutedEventArgs e)
     {
         if (_about == null)
@@ -28,6 +47,24 @@ public partial class MainWindow : Window
     {
         if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
             BeginMoveDrag(e);
+    }
+    private void TitleBar_DoubleTapped(object? sender, TappedEventArgs e)
+    {
+        // 리사이즈 불가면(지금 CanResize=False) 토글 의미 없음
+        // MainWindow에서 토글 쓰려면 CanResize=True 여야 함
+
+        if (WindowState == WindowState.Maximized)
+            WindowState = WindowState.Normal;
+        else
+            WindowState = WindowState.Maximized;
+    }
+    private void UpdatePadding()
+    {
+        // 최대화일 때 보정
+        if (WindowState == WindowState.Maximized)
+            WindowPadding = new Thickness(5);
+        else
+            WindowPadding = new Thickness(0);
     }
 
 }
