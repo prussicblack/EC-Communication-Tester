0f9e5345a6094cc1e4d9472dc94282814bd70949
prussicblack <prussicblack@gmail.com>
2026-04-29 10:23:14 +0900

    다른 프로젝트에 사용할 로그기록 관련 사용법 정리.

--- SOEM_FrontEnd/Util/Logging/Log.cs
+++ SOEM_FrontEnd/Util/Logging/Log.cs
@@ -10,13 +10,45 @@ namespace SOEM_FrontEnd.Util.Logging
 {
     public static class Log
     {
-        //초기화 선언 다 귀찮고 한방에 로그 지를때.
-        
+
+        /// <summary>
+        /// 로깅 기본 사용법.
+        /// 
+        /// 1. 로깅시작 초기화 --> 필수.
+        /// OPLogger.Configure(new OPLoggerOptions
+        /// {
+        ///    LogFolder = "OPLogs",
+        ///    FileNamePrefix = "soem_",
+        ///    MaxFolderBytes = 512L * 1024L * 1024L,   // 512MB
+        ///    //MaxFolderBytes = 128L * 1024L,   // 128KB
+        ///    MaxActiveFileBytes = 32L * 1024L * 1024L, // 32MB
+        ///    //MaxActiveFileBytes = 32L * 1024L, // 32KB
+        ///    QueueCapacity = 8192,
+        ///    QueueMode = OPLogQueueMode.Block,
+        ///    MinimumLevel = LogLevel.Information,
+        ///    EnableFailover = true
+        /// });
+        ///
+        /// 2. 로거 생성 후 
+        /// ILogger log = OPLogger.CreateLogger("App");
+        /// 3. 로거내부 메소드 호출.
+        /// log.LogInformation("App boot");
+        ///
+        /// 생성도 귀찮으면, 로깅 빠른 사용법.
+        /// 1. 초기화는 필수로 해줘야하고...
+        /// OPLogger.Configure(---);
+        /// 
+        /// 2. 선택사항, 기본 카테고리 설정.(비선언 타입 초기화용)
+        /// Log.SetDefaultCategory("SOEM"); 
+        ///
+        /// 3. Static으로 그냥 로그 기록.
+        /// Log.I/W/E 사용.
+        /// 
+        /// </summary>
+
+
         private static ILogger _default;
 
-        //OPLogger.Configure(...);
-        //Log.SetDefaultCategory("SOEM");
-        //이런형식으로 Configure밑에 질러줘야 되긴함.
         public static void SetDefaultCategory(string category)
         {
             if (string.IsNullOrWhiteSpace(category)) category = "App";
