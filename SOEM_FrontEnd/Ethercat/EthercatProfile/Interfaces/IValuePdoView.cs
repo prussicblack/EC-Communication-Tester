4a8e0f98e7303ddf2c8005938b0d76efef8b560d
prussicblack <70947742+prussicblack@users.noreply.github.com>
2026-05-01 11:21:25 +0900

    Analogprofile 작성중.

---
+++ SOEM_FrontEnd/Ethercat/EthercatProfile/Interfaces/IValuePdoView.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOEM_FrontEnd.Ethercat.EthercatProfile.Interfaces
+{
+    public interface IValuePdoView
+    {
+        IReadOnlyList<ValueChannelDefinition> ValueDefinitions { get; }
+
+        long ValueSequence { get; }
+
+        ValueSnapshotFrame GetValueSnapshot();
+
+    }
+
+    public enum ValuePdoDirection
+    {
+        Input = 0,   // TxPDO: slave -> master
+        Output = 1   // RxPDO: master -> slave
+    }
+
+    public sealed class ValueChannelDefinition
+    {
+        public int SlaveNo { get; set; }
+        public int ChannelNo { get; set; }
+
+        public ValuePdoDirection Direction { get; set; }
+
+        public ushort BaseIndex { get; set; }
+
+        public ushort Index { get; set; }
+        public byte SubIndex { get; set; }
+
+        public string Name { get; set; } = "";
+        public string DataType { get; set; } = "";
+
+        public int BitLength { get; set; }
+        public int ByteOffset { get; set; }
+        public int BitInByte { get; set; }
+
+        public ValueRawKind RawKind { get; set; }
+
+        public string AddressText
+        {
+            get
+            {
+                return "0x" + Index.ToString("X4") + ":" + SubIndex.ToString("X2");
+            }
+        }
+    }
+
+    public sealed class ValueSnapshotFrame
+    {
+        public long Sequence { get; set; }
+
+        public ValueChannelSnapshot[] Channels { get; set; }
+
+        public ValueSnapshotFrame()
+        {
+            Channels = new ValueChannelSnapshot[0];
+        }
+    }
+
+    public struct ValueChannelSnapshot
+    {
+        public int SlaveNo;
+        public int ChannelNo;
+
+        // 0 = Input(TxPDO), 1 = Output(RxPDO)
+        public int Direction;
+
+        public ushort Index;
+        public byte SubIndex;
+        public byte RawKind;
+
+        public long RawSigned;
+        public ulong RawUnsigned;
+        public double RawFloat;
+    }
+
+    public enum ValueRawKind
+    {
+        Unknown = 0,
+
+        Int8 = 1,
+        UInt8 = 2,
+
+        Int16 = 3,
+        UInt16 = 4,
+
+        Int32 = 5,
+        UInt32 = 6,
+
+        Int64 = 7,
+        UInt64 = 8,
+
+        Real32 = 9,
+        Real64 = 10
+    }
+
+
+}
---
+++ SOEM_FrontEnd/Ethercat/EthercatProfile/NormalValueProfile.cs
@@ -0,0 +1,77 @@
+﻿using SOEM_FrontEnd.Ethercat.EthercatProfile.Interfaces;
+using SOEM_FrontEnd.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOEM_FrontEnd.Ethercat.EthercatProfile
+{
+    public sealed class NormalValueProfile : PDOBase, IPDOView, IValuePdoView
+    {
+        private readonly List<ValueChannelDefinition> _valueDefinitions =
+            new List<ValueChannelDefinition>();
+
+        private ValueChannelSnapshot[] _snapshots =
+            new ValueChannelSnapshot[0];
+
+        private long _valueSequence;
+
+        public IReadOnlyList<ValueChannelDefinition> ValueDefinitions
+        {
+            get { return _valueDefinitions; }
+        }
+
+        public long ValueSequence
+        {
+            get { return _valueSequence; }
+        }
+
+        public NormalValueProfile(int rxSize, int txSize, ushort slaveNo, EcClient ecClient)
+            : base(rxSize, txSize, slaveNo, ecClient)
+        {
+        }
+
+        public void BuildValueChannels()
+        {
+            _valueDefinitions.Clear();
+
+            BuildFromMapRows(ValuePdoDirection.Output, RxPdoMapRows);
+            BuildFromMapRows(ValuePdoDirection.Input, TxPdoMapRows);
+
+            _snapshots = new ValueChannelSnapshot[_valueDefinitions.Count];
+        }
+
+        public ValueSnapshotFrame GetValueSnapshot()
+        {
+            ValueChannelSnapshot[] copy = new ValueChannelSnapshot[_snapshots.Length];
+            Array.Copy(_snapshots, copy, _snapshots.Length);
+
+            ValueSnapshotFrame frame = new ValueSnapshotFrame();
+            frame.Sequence = _valueSequence;
+            frame.Channels = copy;
+
+            return frame;
+        }
+
+        public void UpdateValueSnapshots()
+        {
+            _valueSequence++;
+
+            for (int i = 0; i < _valueDefinitions.Count; i++)
+            {
+                ValueChannelDefinition definition = _valueDefinitions[i];
+
+                ValueChannelSnapshot snapshot;
+
+                bool ok = TryReadValue(definition, out snapshot);
+
+                if (ok)
+                {
+                    _snapshots[i] = snapshot;
+                }
+            }
+        }
+    }
+}
