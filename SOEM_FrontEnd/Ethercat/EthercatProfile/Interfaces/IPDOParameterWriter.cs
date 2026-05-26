71c061f692148bb18b29d0b12e3383909118b7d5
ursamizar <prussicblack@gmail.com>
2026-05-26 20:52:36 +0900

    누락파일 추가.

---
+++ SOEM_FrontEnd/Ethercat/EthercatProfile/Interfaces/IPDOParameterWriter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOEM_FrontEnd.Ethercat.EthercatProfile.Interfaces
+{
+    public interface IPDOParameterWriter
+    {
+        bool TryWritePdoParameter(ushort index, byte subIndex, long value);
+    }
+}
